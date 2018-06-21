17f9e249a83edb4bc202963563799931f53ba68e
reubenjh <rjharcourt@gmail.com>
2018-06-22 10:01:56 +1200

    added circle props to classes

--- client/components/Baby.jsx
+++ client/components/Baby.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 class Baby extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            cx: props.cx || Math.random()*800,/*width of game screen*/
+            cy: props.cy || Math.random()*600,/*height of game screen*/
+            r: 60,
+            fill: 'blue'
+        }
     }
     render() {
         return (
-            <div>
-                <h1>I'm Baby</h1>
-            </div>
+            <React.Fragment>
+                <circle style={{fill: (this.state.fill)}} cx={this.state.cx} cy={this.state.cy} r={this.state.r}  />
+            </React.Fragment>
         )
     }
 }
--- client/components/Young.jsx
+++ client/components/Young.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 class Young extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            cx: props.cx || Math.random()*800,/*width of game screen*/
+            cy: props.cy || Math.random()*600,/*height of game screen*/
+            r: 90,
+            fill: 'yellow'
+        }
     }
     render() {
         return (
-            <div>
-                <h1>I'm Young</h1>
-            </div>
+            <React.Fragment>
+                <circle style={{fill: (this.state.fill)}} cx={this.state.cx} cy={this.state.cy} r={this.state.r}  />
+            </React.Fragment>
         )
     }
 }
