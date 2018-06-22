fcaaaa52cb2666a239c0fa26821f612625ce4d9a
reubenjh <rjharcourt@gmail.com>
2018-06-22 17:25:07 +1200

    smaller image

--- client/components/Old.jsx
+++ client/components/Old.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Old = props => (
         <g>        
            <circle style={{fill: props.fill}} cx={props.cx} cy={props.cy} r={props.r}  />
-            <image href={props.image} x={props.cx-props.r} y={props.cy-props.r} width={props.r*2} height={props.r*2} />
+            <image href={props.image} x={props.cx-props.r*.8} y={props.cy-props.r*.8} width={props.r*1.6} height={props.r*1.6} />
          
             
         </g>       
