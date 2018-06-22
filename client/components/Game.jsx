eba9a7a8ef0e221ff9bd4ab14226f3113c5ee7e7
reubenjh <rjharcourt@gmail.com>
2018-06-22 14:24:16 +1200

    added reactMouse

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -17,15 +17,29 @@ class Game extends React.Component {
     this.move = this.move.bind(this)
     this.oldCollisions = this.oldCollisions.bind(this)
     this.youngCollisions = this.youngCollisions.bind(this)
+    this.reactMouse = this.reactMouse.bind(this)
   }
 
   move(evt){
     const svgCoords = mouse.mouseTransform(evt.clientX, evt.clientY)
     let {old, young, babies} = this.state    
-    old[0].cx = svgCoords.x
-    old[0].cy = svgCoords.y 
+    this.reactMouse(svgCoords.x, svgCoords.y)    
     young = young.concat(this.oldCollisions(old))
     babies = babies.concat(this.youngCollisions(young))
+    this.setState({old, young, babies})
+  }
+
+  reactMouse(x, y){
+    let {old, young, babies} = this.state
+    for (let i=0; i < old.length; i++){
+      if (mouse.checkMouseDistance(old[i],x,y) < 150) old[i] = mouse.moveFromMouse(old[i])
+    }
+    for (let i=0; i < young.length; i++){
+      if (mouse.checkMouseDistance(young[i],x,y) < 110) young[i] = mouse.moveFromMouse(young[i])
+    }
+    for (let i=0; i < babies.length; i++){
+      if (mouse.checkMouseDistance(babies[i],x,y) <80) babies[i] = mouse.moveFromMouse(babies[i])
+    }
     this.setState({old, young, babies})
   }
 
